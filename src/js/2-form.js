ade8c6de46e7aee3d0f2b3a3dad9f63e5f0a194f
kkaammiillaa <149430081+kkaammiillaa@users.noreply.github.com>
2024-03-22 15:53:22 +0100

    do task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,37 @@
-console.log('form');
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.email');
+const message = document.querySelector('.message');
+const key = 'feedback-form-state';
+
+if (localStorage.getItem(key)) {
+  setPreviousValue();
+}
+const valueObj = JSON.parse(localStorage.getItem(key)) || {};
+
+form.addEventListener('input', saveValue);
+function saveValue(event) {
+  const value = event.target.value;
+  valueObj[event.target.name] = value.trim();
+  localStorage.setItem(key, JSON.stringify(valueObj));
+}
+
+function setPreviousValue() {
+  const previousValues = JSON.parse(localStorage.getItem(key));
+  const previousEmail = previousValues.email;
+  const previousMessage = previousValues.message;
+
+  email.value = previousEmail || '';
+  message.value = previousMessage || '';
+}
+
+form.addEventListener('submit', formSubmit);
+function formSubmit(event) {
+  if (email.value.trim() && message.value.trim()) {
+    event.preventDefault();
+    console.log(valueObj);
+    event.currentTarget.reset();
+    localStorage.removeItem(key);
+  } else {
+    alert('You should fill in the form');
+  }
+}
